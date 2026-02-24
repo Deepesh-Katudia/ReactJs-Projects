0710755a68b5e411760efab41729230974e79450
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-02-24 12:04:05 -0500

    Feat: added component Card

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+
+const components = () => {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+)}
+
+export default components
