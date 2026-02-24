5cafc0e4c43552a2c84476a94f83ec18c798ff0a
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-02-24 12:04:24 -0500

    Feat: Added component crad

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+        <div className="card">
+      <h1>Components</h1>
+      <p>Components are the building blocks of a React application. They allow you to break down your UI into reusable pieces, making it easier to manage and maintain your code.</p>
+    </div>
+  )
+}
+
+export default Card
