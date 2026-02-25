20686d16a29b8d56e72622fb4e1663aad5230f8b
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-02-24 22:45:44 -0500

    Feat: Creation of card component for props

---
+++ 03-props/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+            <div className="card">
+        <img src="https://plus.unsplash.com/premium_photo-1771136519980-fba8883a5a62?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D" alt="" />
+        <h1>Card Title</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        <button>Click Me</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
