7689b19aa8fa746934d37620004f8f52d3746c6a
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-02-25 16:07:42 -0500

    Feat: props example

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
   return (
     <div className='parent'>
-
+      <Card user='Deepesh' age={25} />
+      <Card user='Rahul' age={30} />
+      <Card user='Suresh' age={35} />
     </div> 
   )
 }
