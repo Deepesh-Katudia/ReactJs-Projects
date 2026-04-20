1e5a146d96809b6c0253ea0797b52e65951d808a
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-04-20 16:43:20 -0400

    feat: refactor App component to include User and Card components

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,32 +1,24 @@
 import React from 'react'
 import { Bookmark } from 'lucide-react'
+import  Card  from "./components/Card";
+import User from './components/User';
 
 const App = () => {
+
+  const arr = [1, 2];
+
+
   return (
     <div className='parent'>
-      <div className="card">
-        <div className="top">
-          <img src="https://www.fineprintart.com/images/blog/amazon-logo/amazon_logo_history_5.jpg" alt="" />
-          <button>Save <Bookmark /></button>
-        </div>
-        <div className="center">
-          <h3>Amazon <span>5 Days ago</span></h3>
-            <h2>Senior Software Developer</h2>
-            <div className='tag'>
-              <h4>Part-time </h4>
-              <h4>Senior Level</h4>
-            </div>
-        </div>
-        <div className="bottom">
-          <div>
-            <div>
-              <h3>Salary: $120,000</h3>
-              <p>Mumbai, India</p>
-            </div>
-          </div>
-        </div>
-        <button>Apply Now</button>
-      </div>
+      
+     <Card />
+    <User first="John" last="Doe" />
+    <User first={arr[0]} />
+
+    {arr.map(function(item){
+      return <User first={item} />;
+    })}
+    
     </div>
   )
 }
